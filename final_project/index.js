ee370b247c92820fe307c8adad4dfc7804f8be4f
Stephanie Haber <stephanie.haber@outlook.de>
2024-02-24 15:14:00 -0500

    changes made from theia lab environment

--- final_project/index.js
+++ final_project/index.js
@@ -11,8 +11,59 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
-});
+    if(req.session.authorization) {
+        token = req.session.authorization['accessToken'];
+        jwt.verify(token, "access",(err,user)=>{
+            if(!err){
+                req.user = user;
+                next();
+            }
+            else{
+                return res.status(403).json({message: "User not authenticated"})
+            }
+         });
+     } else {
+         return res.status(403).json({message: "User not logged in"})
+     }
+ });
+ 
+ app.post("/login", (req,res) => {
+   const username = req.body.username;
+   const password = req.body.password;
+ 
+   if (!username || !password) {
+       return res.status(404).json({message: "Error logging in"});
+   }
+ 
+   if (authenticatedUser(username,password)) {
+     let accessToken = jwt.sign({
+       data: password
+     }, 'access', { expiresIn: 60 * 60 });
+ 
+     req.session.authorization = {
+       accessToken,username
+   }
+   return res.status(200).send("User successfully logged in");
+   } else {
+     return res.status(208).json({message: "Invalid Login. Check username and password"});
+   }
+ });
+ 
+ app.post("/register", (req,res) => {
+   const username = req.body.username;
+   const password = req.body.password;
+ 
+   if (username && password) {
+     if (!doesExist(username)) { 
+       users.push({"username":username,"password":password});
+       return res.status(200).json({message: "User successfully registred. Now you can login"});
+     } else {
+       return res.status(404).json({message: "User already exists!"});    
+     }
+   } 
+   return res.status(404).json({message: "Unable to register user."});
+ });
+ 
  
 const PORT =5000;
 

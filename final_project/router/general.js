30b5b8c1b13a87b2d2b2f34c5091bb4cbbd4d49f
stephanieh437 <153853420+stephanieh437@users.noreply.github.com>
2024-02-25 20:35:40 +0100

    Update general.js
    updated the version from theia lab environment, since git push did not work

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -16,15 +16,18 @@ public_users.get('/',function (req, res) {
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
+
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
+public_users.get('/:isbn',function (req, res) {
+    const isbn = req.params.isbn;
+    res.send(books[isbn])
   return res.status(300).json({message: "Yet to be implemented"});
  });
-  
-// Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
+
+ // Get book details based on author
+ public_users.get('/author/:author',function (req, res) {
+    const author = req.params.author;
+    res.send(books[author]);
   return res.status(300).json({message: "Yet to be implemented"});
 });
 

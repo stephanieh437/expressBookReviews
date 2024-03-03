6049a0e76f5ad8159a0be4dea35f2baaf140fae3
stephanieh437 <153853420+stephanieh437@users.noreply.github.com>
2024-03-03 17:22:11 +0100

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,22 +18,24 @@ public_users.get('/',function (req, res) {
 
 
 // Get book details based on ISBN
-public_users.get('/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn',function (req, res) {
     const isbn = req.params.isbn;
     res.send(books[isbn])
   return res.status(300).json({message: "Yet to be implemented"});
  });
 
- // Get book details based on author
- public_users.get('/author/:author',function (req, res) {
-    const author = req.params.author;
+  // Get book details based on author
+  public_users.get('/author/:author',function (req, res) {
+    const isbn = req.params.isbn;
+    let author = req.body.author;
     res.send(books[author]);
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
+    const title = req.params.title;
+    res.send(books[title]);
   return res.status(300).json({message: "Yet to be implemented"});
 });
 

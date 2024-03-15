bb8e70d4e5ed5986702a431eac9cd9d7e1573f09
stephanieh437 <153853420+stephanieh437@users.noreply.github.com>
2024-03-15 17:55:35 +0100

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
-const users = [];
+let users = [];
 
 const isValid = (username)=>{ 
     let userswithsamename = users.filter((user)=>{
@@ -30,11 +30,43 @@ const authenticatedUser = (username,password)=>{
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
+        const username = req.body.username;
+        const password = req.body.password;
+      
+        if (!username || !password) {
+            return res.status(404).json({message: "Error logging in"});
+        }
+      
+        if (authenticatedUser(username,password)) {
+          let accessToken = jwt.sign({
+            data: password
+          }, 'access', { expiresIn: 60 * 60 });
+      
+          req.session.authorization = {
+            accessToken,username
+        }
+        return res.status(200).send("User successfully logged in");
+        } else {
+          return res.status(208).json({message: "Invalid Login because why not. Check username and password"});
+        };
  return res.status(300).json({message: "Yet to be implemented"});
 });
 
-
+//register user
+regd_users.post("/register", (req,res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    if (username && password) {
+      if (!isValid(username)) { //changed doesExist to isValid for Task 6 on March 11, removed ! from !isValid for Task 6 on March 15
+        users.push({"username":username,"password":password});
+        return res.status(200).json({message: "User successfully registred. Now you can login"});
+      } else {
+        return res.status(404).json({message: "User already exists!"});    
+      }
+    } 
+    return res.status(404).json({message: "Unable to register user."});
+  });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
